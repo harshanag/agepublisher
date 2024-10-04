33b28fd9121ef94fd5ea389842b1c8813ef0be80
Harshana Gurusingha <harshanagurusingha@Harshanas-MacBook-Pro.local>
2024-10-04 23:18:57 +1000

    Removed conflicting versions in pom

--- agepublisher/src/test/java/com/demo/agepublisher/AgepublisherApplicationTests.java
+++ agepublisher/src/test/java/com/demo/agepublisher/AgepublisherApplicationTests.java
@@ -1,14 +1,53 @@
 package com.demo.agepublisher;
 
+import com.demo.agepublisher.enums.KafkaTopics;
+import com.demo.agepublisher.model.Person;
+import com.demo.agepublisher.service.PersonDetailsPublishService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.time.LocalDate;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 class AgepublisherApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Mock
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	@InjectMocks
+	private PersonDetailsPublishService personDetailsPublishService;
+
+	public AgepublisherApplicationTests() {
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	public void testConsumeMessageEvenAge() {
+		Person person = new Person("John", "Doe", LocalDate.of(1990,1,1));
+		personDetailsPublishService.sendPersonDataToKafka(person);
+
+		verify(kafkaTemplate, times(1)).send(eq(KafkaTopics.CUSTOMER_EVEN.getTopicName()), anyString());
+	}
+
+	@Test
+	public void testConsumeMessageOddAge() {
+		Person person = new Person("John", "Doe", LocalDate.of(1991,1,1));
+		personDetailsPublishService.sendPersonDataToKafka(person);
+
+		verify(kafkaTemplate, times(1)).send(eq(KafkaTopics.CUSTOMER_ODD.getTopicName()), anyString());
 	}
 
 }
