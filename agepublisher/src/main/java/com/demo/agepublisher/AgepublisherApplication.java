246f1be06375968c8487873158c278823e4eb655
Harshana Gurusingha <harshanagurusingha@Harshanas-MacBook-Pro.local>
2024-10-04 22:26:59 +1000

    Added direct runner

--- agepublisher/src/main/java/com/demo/agepublisher/AgepublisherApplication.java
+++ agepublisher/src/main/java/com/demo/agepublisher/AgepublisherApplication.java
@@ -3,6 +3,7 @@ package com.demo.agepublisher;
 import com.demo.agepublisher.enums.KafkaTopics;
 import com.demo.agepublisher.functions.AgeProcessingFunction;
 import com.demo.agepublisher.functions.MessagePublishFunction;
+import org.apache.beam.runners.direct.DirectRunner;
 import org.apache.beam.sdk.io.kafka.KafkaIO;
 import org.apache.beam.sdk.options.*;
 import org.apache.beam.sdk.transforms.ParDo;
@@ -10,18 +11,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.coders.StringUtf8Coder;
-import org.apache.beam.sdk.transforms.Create;
-import org.apache.beam.sdk.transforms.MapElements;
-import org.apache.beam.sdk.values.PCollection;
-import org.apache.beam.sdk.values.TypeDescriptors;
 
 @SpringBootApplication
 public class AgepublisherApplication {
 
 	public static void main(String[] args) {
 		PipelineOptions options = PipelineOptionsFactory.create();
-		options.setRunner(org.apache.beam.runners.flink.FlinkRunner.class);
+		options.setRunner(DirectRunner.class);
 		Pipeline pipeline = Pipeline.create(options);
 
 		String kafkaBootstrapServers = "localhost:9092";
